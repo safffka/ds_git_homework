25b0047ba1dd62934a4745809910e66f850a1a19
SAVELII SPITSIN <81923063+safffka@users.noreply.github.com>
2025-12-21 09:52:28 +0400

    Revert "chore: add flake8, mypy strict and pre-commit"

--- tests/test_ds_git_homework.py
+++ tests/test_ds_git_homework.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python
+import pytest
+
 """Tests for `ds_git_homework` package."""
 
-import pytest
+# from ds_git_homework import ds_git_homework
 
 
 @pytest.fixture
-def response() -> None:
+def response():
     """Sample pytest fixture.
 
-    See more at:
-    http://doc.pytest.org/en/latest/fixture.html
+    See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    return None
+    # import requests
+    # return requests.get('https://github.com/audreyfeldroy/cookiecutter-pypackage')
 
 
-def test_content(response: None) -> None:
+def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
-    assert response is None
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
